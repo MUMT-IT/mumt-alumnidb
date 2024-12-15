a836707b027e7a491452c73c07c396591a5ae17d
Likit Preeyanon <preeyano@msu.edu>
2024-12-15 12:04:40 +0700

    added database migration
    updated the menu

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('://', 'ql://', 1)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 from app.main import main_blueprint
